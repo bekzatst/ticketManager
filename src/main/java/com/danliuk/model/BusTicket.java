0a0e5c7384fcbebcf50bfb3d1ba59e49f6cbfd9d
Bekzat <117571888+bekzatst@users.noreply.github.com>
2024-08-01 21:37:51 +0500

    initial commit

---
+++ src/main/java/com/danliuk/model/BusTicket.java
@@ -0,0 +1,19 @@
+package com.danliuk.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class BusTicket {
+
+    private String ticketClass;
+
+    private String ticketType;
+
+    private String startDate;
+
+    private String price;
+}
